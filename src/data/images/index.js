6837e0988e2709d59066a180814bc9dfc5bf9607
Bonaventure Chukwudi <bonarhyme@gmail.com>
2022-10-29 19:41:17 +0100

    Added images and logos

---
+++ src/data/images/index.js
@@ -0,0 +1,7 @@
+export { default as Bonaventure } from "./Bonaventure.jpg";
+export { default as github } from "./github.svg";
+export { default as I4G } from "./I4G.svg";
+export { default as shareButton } from "./share-button.svg";
+export { default as shareButton2 } from "./share-button2.svg";
+export { default as slack } from "./slack.svg";
+export { default as ZuriInternship } from "./Zuri.Internship_Logo.svg";
